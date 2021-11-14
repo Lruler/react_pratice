54ab6459f5b208c97eb2594c890a173f032a4bfd
Lruler <1204312199@qq.com>
2021-11-14 12:30:51 +0800

    hooks

---
+++ react_hooks/index.tsx
@@ -0,0 +1,25 @@
+// 自定义Hooks处理表单
+import React, { useState, useCallback } from 'react'
+
+// 基本实现
+const useForm = (initialValues = {}) => {
+
+        // 设置整个form的状态values
+    
+        const [values, setValues] = useState(initialValues)
+    
+        const setFieldValue = useCallback((name,value) => {
+    
+            setValues(value => ({
+    
+                ...values,
+    
+                [name]: value
+    
+            }))
+    
+        }, [])
+    
+        return { values, setFieldValue }
+    
+    }
